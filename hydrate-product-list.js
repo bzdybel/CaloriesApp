9f1f64835b6ee64c6027edffb23e291ed4a773b7
BartłomiejZdybel <bartlomiej.zdybel@gmail.com>
2019-01-14 22:10:45 +0100

    rename js file, change kcal name, delete useless gap

--- hydrate-product-list.js
+++ hydrate-product-list.js
@@ -1,22 +1,22 @@
 const products = [{
         name: "Apple",
-        kcalper100g: "52"
+        kcalPer100g: "52"
     },
     {
         name: "Egg",
-        kcalper100g: "140"
+        kcalPer100g: "140"
     },
     {
         name: "Banana yoghurt",
-        kcalper100g: "86"
+        kcalPer100g: "86"
     },
     {
         name: "Chicken and rice",
-        kcalper100g: "79"
+        kcalPer100g: "79"
     },
     {
         name: "Orange",
-        kcalper100g: "79"
+        kcalPer100g: "79"
     },
 
 ];
@@ -28,15 +28,13 @@ products.forEach(product => {
     const productListItem = document.createElement("li");
     productListItem.classList.add("product-list__item");
 
-
-
     const productTitle = document.createElement("div");
     productTitle.classList.add("product-list__title");
     productTitle.innerText = product.name;
 
     const productDescription = document.createElement("small");
     productDescription.classList.add("product-list__description");
-    productDescription.innerText = `${product.kcalper100g} kcal per 100g`;
+    productDescription.innerText = `${product.kcalPer100g} kcal per 100g`;
 
     productListItem.appendChild(productTitle);
     productListItem.appendChild(productDescription);
