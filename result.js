ebd54e5c31a8bb4062a34345caafa37d698b3d7c
BartłomiejZdybel <bartlomiej.zdybel@gmail.com>
2019-01-17 22:44:20 +0100

    complete result section

--- result.js
+++ result.js
@@ -1,17 +1,22 @@
-const productListDropdown = document.querySelector(".add-product-form__dropdown");
 const productKcalInfo = document.querySelector(".add-product-form__kcal-info");
 const productKcalOutcome = document.querySelector(".add-product-form__kcal-outcome");
+const productInput = document.querySelector(".add-product-form__input");
 
-productListDropdown.addEventListener("onchange", resultFunciton() {
-    let currentProduct = document.querySelector(".add-product-form__dropdown").value;
-    let currentAmount = document.querySelector(".add-product-form__input");
 
-    products.forEeach(product => {
-        if (product.value === currentProduct) {
-            let result = parseInt(product.kcalPer100g) * currentAmount.value / 100;
+const updateKcalResult = event => {
 
-            productKcalOutcome.innerText = `results in ${result}`;
-            productKcalInfo.innerText = `${product.kcalPer100g} kcal per 100g`;
-        }
-    })
-}, false);
+    const currentProductValue = document.querySelector(".add-product-form__dropdown").value;
+    const currentAmountValue = document.querySelector(".add-product-form__input").value;
+
+    const currentProduct = products.find(product => product.value === currentProductValue);
+
+    const kcalPer100gText = currentProduct ? currentProduct.kcalPer100g : "x";
+    const resultInX = currentProduct ? parseInt(currentProduct.kcalPer100g) * currentAmountValue / 100 : "x";
+
+    productKcalInfo.innerText = `${kcalPer100gText} kcal per 100g`;
+    productKcalOutcome.innerText = `results in ${resultInX} kcal`;
+
+}
+updateKcalResult();
+productListDropdown.addEventListener("change", updateKcalResult);
+productInput.addEventListener("change", updateKcalResult);
