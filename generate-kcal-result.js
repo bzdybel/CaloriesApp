e89827f3dde40180eb82ea27df0b13a6fde07061
BartłomiejZdybel <bartlomiej.zdybel@gmail.com>
2019-01-18 16:14:48 +0100

    change names of files and variables

--- generate-kcal-result.js
+++ generate-kcal-result.js
@@ -1,22 +1,22 @@
 const productKcalInfo = document.querySelector(".add-product-form__kcal-info");
 const productKcalOutcome = document.querySelector(".add-product-form__kcal-outcome");
-const productInput = document.querySelector(".add-product-form__input");
+const productAmountInput = document.querySelector(".add-product-form__input");
 
 
 const updateKcalResult = event => {
 
     const currentProductValue = document.querySelector(".add-product-form__dropdown").value;
-    const currentAmountValue = document.querySelector(".add-product-form__input").value;
+    const currentProductAmountValue = document.querySelector(".add-product-form__input").value;
 
     const currentProduct = products.find(product => product.value === currentProductValue);
 
     const kcalPer100gText = currentProduct ? currentProduct.kcalPer100g : "x";
-    const resultInX = currentProduct ? parseInt(currentProduct.kcalPer100g) * currentAmountValue / 100 : "x";
+    const resultInKcalText = currentProduct ? currentProduct.kcalPer100g * currentProductAmountValue / 100 : "x";
 
     productKcalInfo.innerText = `${kcalPer100gText} kcal per 100g`;
-    productKcalOutcome.innerText = `results in ${resultInX} kcal`;
+    productKcalOutcome.innerText = `results in ${resultInKcalText} kcal`;
 
 }
 updateKcalResult();
 productListDropdown.addEventListener("change", updateKcalResult);
-productInput.addEventListener("change", updateKcalResult);
+productAmountInput.addEventListener("change", updateKcalResult);
