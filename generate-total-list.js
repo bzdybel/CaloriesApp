a9bfec918ebf267fbdab593fb4530e63d231b087
bartlomiej <bartlomiej.zdybel@gmail.com>
2019-02-27 17:20:05 +0100

    debug product name

--- generate-total-list.js
+++ generate-total-list.js
@@ -1,19 +1,24 @@
 const addProductButton = document.querySelector(".add-product-form__button");
+const chosenProductAmountValue = document.querySelector(".add-product-form__input").value;
+const chosenProductListWrapper = document.querySelector(".chosen-product-list__wrapper");
 
 const addProduct = event => {
-    const currentProductName = document.querySelector(".add-product-form__dropdown").value;
-    const chosenProductAmountValue = document.querySelector(".add-product-form__input").value;
-    const chosenProductListItemDescriptionWrapper = document.querySelector(".chosen-product-list__item-description-wrapper");
-    const chosenProductListWrapper = document.querySelector(".chosen-product-list__wrapper");
+    let currentProductName = document.querySelector(".add-product-form__dropdown").value;
+    
+    products.forEach(product =>{
+        product.value === currentProductName ?  currentProductName= product.name : "";
+    })
 
     const chosenProduct = document.createElement("li");
     const chosenProductTitle = document.createElement("div");
+    const chosenProductListItemDescriptionWrapper =document.createElement("div");
     const chosenProductAmount = document.createElement("div");
     const chosenProductValue = document.createElement("div");
 
     chosenProduct.classList.add("chosen-product-list__item");
     chosenProductTitle.classList.add("chosen-product-list__item-title");
     chosenProductAmount.classList.add("chosen-product-list__item-description", "chosen-product-list__item-description--weight");
+    chosenProductListItemDescriptionWrapper.classList.add("chosen-product-list__item-description-wrapper");
     chosenProductValue.classList.add("chosen-product-list__item-description");
 
     chosenProductTitle.innerText = currentProductName;
@@ -25,9 +30,10 @@ const addProduct = event => {
     chosenProductListItemDescriptionWrapper.appendChild(chosenProductValue);
     chosenProduct.appendChild(chosenProductListItemDescriptionWrapper);
 
+
+
     chosenProductListWrapper.appendChild(chosenProduct);
-
-
 }
+
 addProduct();
 addProductButton.addEventListener("click", addProduct);
