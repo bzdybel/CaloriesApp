f8e44940041a64fc2682b50240267037d9afe93c
bartlomiej <bartlomiej.zdybel@gmail.com>
2019-02-27 10:33:18 +0100

    debug innertext

--- generate-total-list.js
+++ generate-total-list.js
@@ -1,30 +1,33 @@
 const addProductButton = document.querySelector(".add-product-form__button");
-const chosenProductListItemDescriptionWrapper = document.querySelector(".chosen-product-list__item-description-wrapper");
 
 const addProduct = event => {
     const currentProductName = document.querySelector(".add-product-form__dropdown").value;
-    const choosenProductAmountValue = document.querySelector(".add-product-form__input").value;
+    const chosenProductAmountValue = document.querySelector(".add-product-form__input").value;
+    const chosenProductListItemDescriptionWrapper = document.querySelector(".chosen-product-list__item-description-wrapper");
+    const chosenProductListWrapper = document.querySelector(".chosen-product-list__wrapper");
 
-    const choosenProduct = document.createElement("li");
-    const choosenProductTitle = document.createElement("div");
-    const choosenProductAmount = document.createElement("div");
-    const choosenProductValue = document.createElement("div");
+    const chosenProduct = document.createElement("li");
+    const chosenProductTitle = document.createElement("div");
+    const chosenProductAmount = document.createElement("div");
+    const chosenProductValue = document.createElement("div");
 
-    choosenProduct.classList.add("chosen-product-list__item");
-    choosenProductTitle.classList.add("chosen-product-list__item-title");
-    choosenProductAmount.classList.add("chosen-product-list__item-description", "chosen-product-list__item-description--weight");
-    choosenProductValue.classList.add("chosen-product-list__item-description");
+    chosenProduct.classList.add("chosen-product-list__item");
+    chosenProductTitle.classList.add("chosen-product-list__item-title");
+    chosenProductAmount.classList.add("chosen-product-list__item-description", "chosen-product-list__item-description--weight");
+    chosenProductValue.classList.add("chosen-product-list__item-description");
 
-    choosenProductTitle.innerText(currentProductName);
-    choosenProductAmount.innerText(choosenProductAmountValue);
-    choosenProductValue.innerText(resultInKcalText); // generate-kcal-result ??
+    chosenProductTitle.innerText = currentProductName;
+    chosenProductAmount.innerText = chosenProductAmountValue;
+    chosenProductValue.innerText = '100'; // generate-kcal-result ??
 
-    choosenProduct.appendChild(choosenProductTitle);
-    chosenProductListItemDescriptionWrapper.appendChild(choosenProductAmount);
-    chosenProductListItemDescriptionWrapper.appendChild(choosenProductValue);
-    choosenProduct.appendChild(chosenProductListItemDescriptionWrapper);
+    chosenProduct.appendChild(chosenProductTitle);
+    chosenProductListItemDescriptionWrapper.appendChild(chosenProductAmount);
+    chosenProductListItemDescriptionWrapper.appendChild(chosenProductValue);
+    chosenProduct.appendChild(chosenProductListItemDescriptionWrapper);
+
+    chosenProductListWrapper.appendChild(chosenProduct);
 
 
 }
 addProduct();
-addProductButton.addEventListener("sumbit", addProduct);
+addProductButton.addEventListener("click", addProduct);
